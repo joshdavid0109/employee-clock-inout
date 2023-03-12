a66741ab5017a88e8505381ee7d82ac1c8447859
reybans <2225189@slu.edu.ph>
2023-03-12 08:23:55 +0800

    gson implementation done

--- src/main/java/org/mainfolder/Employee.java
+++ src/main/java/org/mainfolder/Employee.java
@@ -7,5 +7,54 @@ public class Employee {
     int age;
     boolean isLoggedIn;
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+
     public Employee() {}
+
+    public Employee(String fn, String ln, int a, boolean ili) {
+        this.firstName = fn;
+        this.lastName = ln;
+        this.age = a;
+        this.isLoggedIn = ili;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", isLoggedIn=" + isLoggedIn +
+                '}';
+    }
 }
