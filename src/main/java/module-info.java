36fb53165ab7e8cf0637b8bdaf0612567931b22f
Marko <2212093@slu.edu.ph>
2023-03-17 07:53:22 +0800

    Import JavaFx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module RMI.Server {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+    requires java.rmi;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    requires com.dlsc.formsfx;
+
+    requires com.google.gson;
+}
